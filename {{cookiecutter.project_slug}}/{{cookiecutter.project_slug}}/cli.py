237d1eb0e516eaa5688d0a6e51a9e125bb732f9a
viseshrp <viseshrprasad@gmail.com>
2018-12-31 13:35:01 -0500

    fix imports

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
 """Console script for {{cookiecutter.project_slug}}."""
-import sys
 import click
 
 from .{{cookiecutter.project_slug}} import run
+
 
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
 def main(args=None):
@@ -20,7 +20,6 @@ def main(args=None):
     except Exception as e:
         # all other exceptions
         raise click.ClickException(e)
-
 
 
 if __name__ == "__main__":
